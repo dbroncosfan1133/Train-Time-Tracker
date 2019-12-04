37b5703d98615e6c552284a80afa39cc46ae5efd
Max Power <dbroncosfan@gmail.com>
2019-12-03 18:39:42 -0700

    code cleanup and remove console.logs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -46,33 +46,25 @@ $(document).ready(function(){
     });
 
 //Run user input through moment.js to calculate train times
-    database.ref().on("child_added", function(childSnapshot) {
-
-console.log(childSnapshot.val());
+    database.ref().on("child_added", function(childSnapshot) {;
 
     // First Time (pushed back 1 year to make sure it comes before current time)
     var firstTimeConverted = moment(childSnapshot.val().trainFirstTime, "HH:mm").subtract(1, "years");
-    console.log(firstTimeConverted.format("llll"));
 
     // Current Time
     var currentTime = moment();
-    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
 
     // Difference between the times
     var diffTime = currentTime.diff(firstTimeConverted, "minutes");
-    console.log("DIFFERENCE IN TIME: " + diffTime);
 
     // Time apart (remainder)
     var trainRemainder = diffTime % childSnapshot.val().trainFrequency;
-    console.log(trainRemainder);
 
     // Minutes Until Train
     var minutesTilTrain = childSnapshot.val().trainFrequency - trainRemainder;
-    console.log("MINUTES TILL TRAIN: " + minutesTilTrain);
 
     // Next Train minutes away
     var nextTrain = currentTime.add(minutesTilTrain, "minutes");
-    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
     
     //Next trains arrival time
     var arrivalTime = nextTrain.format("hh:mm a");
@@ -89,7 +81,4 @@ console.log(childSnapshot.val());
         console.log("Errors handled: " + errorObject.code);
     });
 
-    $(document).ready(function() {
-        
-    });
 });
